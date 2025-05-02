d461581e25c5d48f355beb8afc3a4479a2300dc6
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-05-01 23:07:59 -0500

    updated testing files

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -14,12 +14,7 @@
 import { mount } from 'cypress/react';
 import Quiz from '../../client/src/components/Quiz'
 
-// describe('Quiz Component', () => {
-//   it('renders the start button', () => {
-//     mount(<Quiz />);
-//     cy.contains('Start Quiz').should('exist');
-//   });
-// });
+
 describe('Quiz Component', ()=> {
     beforeEach(()=> {
         cy.intercept({
@@ -32,29 +27,30 @@ describe('Quiz Component', ()=> {
         }
         ).as('getRandomQuestion')
     } )
+    it('should start the quiz and and display the first question', () => {
+        mount(<Quiz />);
+        cy.get('button').contains('Start Quiz').click()
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
+    });
+    
+    it('should answer questions and complete the quiz', ()=> {
+        mount(<Quiz />);
+        cy.get('button').contains('Start Quiz').click()
+        cy.get('button').contains('1').click();
+        cy.get('alert-success').should('be.visible').and('contain', 'Your Score')
+    });
+    
+    it('should start the quiz again after completion', ()=> {
+        mount(<Quiz />);
+        cy.get('button').contains('Start Quiz').click();
+        cy.get('button').contains('1').click();
+        cy.get('button').contains('Take New Quiz').click();
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
+     
+    })
 })
 
 
-it('should start the quiz and and display the first question', () => {
-    mount(<Quiz />);
-    cy.get('button').contains('Start Quiz').click()
-    cy.get('.card').should('be.visible');
-    cy.get('h2').should('not.be.empty');
-});
 
-it('should answer questions and complete the quiz', ()=> {
-    mount(<Quiz />);
-    cy.get('button').contains('Start Quiz').click()
-    cy.get('button').contains('1').click();
-    cy.get('alert-success').should('be.visible').and('contain', 'Your Score')
-});
-
-it('should start the quiz again after completion', ()=> {
-    mount(<Quiz />);
-    cy.get('button').contains('Start Quiz').click();
-    cy.get('button').contains('1').click();
-    cy.get('button').contains('Take New Quiz').click();
-    cy.get('.card').should('be.visible');
-    cy.get('h2').should('not.be.empty');
- 
-})
---
+++ cypress/e2e/quiz.cy.ts
@@ -0,0 +1,34 @@
+describe('Quiz Component', ()=> {
+    beforeEach( ()=> {
+        cy.visit('/')
+    });
+
+    it('should start the quiz and and display the first question', () => {
+        
+        cy.get('button').contains('Start Quiz').click()
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
+    });
+    
+    it('should answer questions and complete the quiz', ()=> {
+        
+        cy.get('button').contains('Start Quiz').click()
+        for (let i = 0; i < 10; i++) {
+            cy.get('button').contains('1').click();
+            
+        }
+        cy.get('alert-success').should('be.visible').and('contain', 'Your Score')
+    });
+    
+    it('should start the quiz again after completion', ()=> {
+        
+        cy.get('button').contains('Start Quiz').click();
+        cy.get('button').contains('1').click();
+        cy.get('button').contains('Take New Quiz').click();
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
+     
+    })
+
+
+})
